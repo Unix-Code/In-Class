9174208b0af9307980c061cdfca41197a99caeee
Unix-Code <dmalakh@comcast.net>
2015-12-15 17:29:17 -0500

    Updated Message.java
    Finalized encrypt() and decrypt() and added getDecryption().
    
    Also added main method.

--- Message.java
+++ Message.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Message {
     private boolean encrypted;
     private String message;
@@ -7,16 +8,25 @@ public class Message {
             this.message = encrypt(message);
             this.encrypted = true;
         }
+        else {
+            this.message = message;
+        }
     }
 
-    public static String encrypt(String msg) {
+    public static void main(String[] args) {
+        Message aMsg = new Message("     qwertyuiopasdfghjklzxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM`1234567890-=[]\\;',./~!@#$%^&*()_+{}|:<>?", false);
+        System.out.println(aMsg.getDecryption());
+    }
+
+    private String encrypt(String msg) {
         StringBuilder end = new StringBuilder();
         StringBuilder temp = new StringBuilder();
 
         for (int i = 0; i < msg.length(); i++) {
-            temp.append(String.valueOf((int)(msg.charAt(i))));
+            temp.append(String.valueOf((int)msg.charAt(i)));
             temp.reverse();
-            String add = (Math.random()*10 + 1 <= i) ? "0 - " + String.valueOf(Integer.parseInt(temp.toString())) : String.valueOf(Integer.parseInt(temp.toString()));
+            // Determines random chance of adding null (can be changed to preference)
+            String add = (Math.random()*(i+10) + 1 <= i) ? "0 - " + temp.toString() : temp.toString();
             end.append(add + " - ");
             temp.delete(0, temp.length());
         }
@@ -24,21 +34,38 @@ public class Message {
         return end.toString();
     }
 
-    public static String[] decrypt(String msg) {
-        StringBuilder end = new StringBuilder();
-        StringBuilder temp = new StringBuilder();
-        String[] theMsg = msg.split(" - ");/*
-        for (int i = 0; i < theMsg.length; i++) {
-        temp.append(theMsg[i]);
-        temp.reverse();
-        end.append((char)(Integer.parseInt(temp.toString())));
-        temp.delete(0, temp.length());
+    private String decrypt(String msg) {
+        if (encrypted == false) {
+            System.err.print("This should never have happened. Message not encrypted.");
+            return message;
         }
 
-        return end.toString();*/
-        for (String word : theMsg) {
-            System.out.println(word);
+        StringBuilder end = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        String[] theMsg = msg.split(" - ");
+
+        try {
+            for (int i = 0; i < theMsg.length; i++) {
+                temp.append(theMsg[i]);
+                temp.reverse();
+                String append = (Integer.parseInt(temp.toString()) == 0) ? "" : String.valueOf((char)Integer.parseInt(temp.toString()));
+                end.append(append);
+                temp.delete(0, temp.length());
+            }
+        } catch (NumberFormatException e) {
+            System.err.print("Error: String was wrongfully set to encrypted\nString has been encrypted");
+            message = encrypt(message);
+            // only saves non-number-only messages
         }
-        return theMsg;
+        return end.toString();
+    }
+
+    public String getDecryption() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Passcode:");
+        String cmd = scan.nextLine();
+        boolean correctPasscode = cmd.equalsIgnoreCase(decrypt("28 - 101 - 811 - 101 - 411 - 511 - 101 - 0 - 23 - 0 - 56 - 511 - 99 - 0 - 501 - 0 - 501"));
+        System.out.print("\fPassword Accepted\n\n");
+        return (correctPasscode) ? decrypt(message) : "Incorrect Password - Failure to Decrypt";
     }
 }
