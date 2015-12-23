a3e25a64227ccfcc3a738675fdd439c5ac9bd6f0
Unix-Code <dmalakh@comcast.net>
2015-12-23 13:41:02 -0500

    Updates

--- Encryptor.java
+++ Encryptor.java
@@ -2,31 +2,38 @@ public class Encryptor {
     public static Message encrypt(Message sStart, String theKey) {
         Message message;
         StringBuilder encryption = new StringBuilder();
-        String start = sStart.getMessage();
+        String start = sStart.getMessage().toLowerCase();
         String key = generateKey(sStart.getMessage(), theKey);
         for (int i = 0; i < start.length(); i++) {
             if (Character.isLetter(start.charAt(i)) == false) {
-                char nonLetter = (start.charAt(i) == ' ') ? 'A' : ((start.charAt(i) >= '0' && start.charAt(i/2) <= '9') ? (char)(start.charAt(i) - '0' + 'B') : start.charAt(i));
+                char nonLetter = (start.charAt(i) == ' ') ? Character.toUpperCase((char)sStart.getLanguage().alphabet[0]) : ((start.charAt(i) >= '0' && start.charAt(i/2) <= '9') ? (char)(start.charAt(i) - '0' + Character.toUpperCase((char)sStart.getLanguage().alphabet[1])) : start.charAt(i));
                 encryption.append(nonLetter);
                 continue;
             }
 
-            encryption.append((char)(((((int)start.charAt(i) - (int)sStart.getLanguage().alphabet[0]) + ((int)key.charAt(i) - (int)sStart.getLanguage().alphabet[0]))%26) + (int)sStart.getLanguage().alphabet[0]));
+            encryption.append((char)(((((int)start.charAt(i) - (int)sStart.getLanguage().alphabet[0]) + ((int)key.charAt(i) - (int)sStart.getLanguage().alphabet[0]))%sStart.getLanguage().alphabet.length) + (int)sStart.getLanguage().alphabet[0]));
         }
 
         return new Message(encryption.toString(), sStart.getLanguage(), true);
     }
 
-    /*
-    public static Message encrypt(String start, Language lang) {
-    Message msg = new Message(start, lang, false);
-    msg = encrypt(msg);
-    return msg;
-    }
-     */
+    public static Message decrypt(Message sStart, String theKey) {
+        Message message;
+        StringBuilder decryption = new StringBuilder();
+        String start = sStart.getMessage();
+        String key = generateKey(sStart.getMessage(), theKey);
+        
+        for (int i = 0; i < start.length(); i++) {
+            if (Character.isLetter(start.charAt(i)) == false || start.charAt(i) < sStart.getLanguage().alphabet[0]) {
+                char nonLetter = (start.charAt(i) == Character.toUpperCase((char)sStart.getLanguage().alphabet[0])) ? ' ' : ((start.charAt(i) >= Character.toUpperCase((char)sStart.getLanguage().alphabet[1]) && start.charAt(i) <= (Character.toUpperCase((char)sStart.getLanguage().alphabet[1]) + 10)) ? (char)((int)start.charAt(i) - (int)Character.toUpperCase((char)sStart.getLanguage().alphabet[1]) + (int)'0') : start.charAt(i));
+                decryption.append(nonLetter);
+                continue;
+            }
 
-    public static Message decrypt(Message start) {
-        return null;
+            decryption.append((char)(((((int)start.charAt(i) - (int)sStart.getLanguage().alphabet[0]) - ((int)key.charAt(i) - (int)sStart.getLanguage().alphabet[0]) + sStart.getLanguage().alphabet.length)%sStart.getLanguage().alphabet.length) + (int)sStart.getLanguage().alphabet[0]));
+        }
+        
+        return new Message(decryption.toString(), sStart.getLanguage(), false);
     }
     
     private static String generateKey(String start, String sKey) {
